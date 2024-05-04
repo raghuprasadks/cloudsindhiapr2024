aa5961d9cf2a31ed3fc916594a1d7dc04e7120a6
raghuprasadks <prasadraghuks@gmail.com>
2024-05-04 09:58:28 +0530

    updated

--- cloud/pythonflask/app.py
+++ cloud/pythonflask/app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def hello():
     return "Welcome to python"
 
+@app.route('/welcome')
+def welcome():
+    return "I am learning flask"
+
+
 if (__name__ == "__main__"):
     app.run(port=5000, debug=True)
 
